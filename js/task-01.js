4970456d988a0d2f326374e990c22ccbb1728277
HannaNikola <h.nikolaienko32@gmail.com>
2023-01-05 03:37:45 +0100

    add-task-2-task-3

--- js/task-01.js
+++ js/task-01.js
@@ -13,11 +13,11 @@ const categoriesEl = document.querySelector("#categories");
 console.log("Number of categories:",categoriesEl.children.length); 
  
 const itemEl = document.querySelectorAll('.item');
-for (const item of itemEl) {
+ for (const item of itemEl) {
    console.log("Category:", item.firstElementChild.textContent);
    console.log("Elements:", item.lastElementChild.children.length);
 
-}
+ }
 
 
 
--- js/task-02.js
+++ js/task-02.js
@@ -3,19 +3,36 @@
 // <ul id="ingredients"></ul>
 
 // JavaScript містить масив рядків.
-
-// const ingredients = [
-//   "Potatoes",
-//   "Mushrooms",
-//   "Garlic",
-//   "Tomatos",
-//   "Herbs",
-//   "Condiments",
-// ];
-
 // Напиши скрипт, який для кожного елемента масиву ingredients:
 
 // Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
 // Додасть назву інгредієнта як його текстовий вміст.
 // Додасть елементу клас item.
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients
+
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+  
+ const ingredientsEl = document.querySelector('#ingredients');
+for (const el of ingredients) {
+  const listEl = document.createElement('li');
+  listEl.classList.add('item');
+  listEl.textContent = ingredients;
+  
+  ingredientsEl.append(listEl)
+};
+
+
+console.log(ingredientsEl);
+
+
+
+
+
+
